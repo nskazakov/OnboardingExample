e7b6896dc9ebcd0a935bf6e71f01340dc6aad1db
Nikita Kazakov <nikita.kazakov95@gmail.com>
2021-08-13 13:24:31 +0800

    Initial Commit

---
+++ OnboardingExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnboardingExample
+//
+//  Created by Nikita Kazakov on 13.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
